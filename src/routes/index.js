5ba6d793e082f814e774f37d644a622c3d3b51f5
jihun1233 <leejihun1233@gmail.com>
2022-05-08 03:06:12 +0900

    🚧 메인화면 레이아웃 수정

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,23 @@
 import styles from './Routes.module.scss'
-import TodoList from './TodoList'
+import Toggle from '../components/Toggle'
+import Tab from '../components/Tab'
+import Slider from '../components/Slider'
+import Input from '../components/Input'
+import Dropdown from '../components/Dropdown'
 
 function App() {
   return (
     <div className={styles.app}>
-      <TodoList />
+      <h1>Toggle</h1>
+      <Toggle />
+      <h1>Tab</h1>
+      <Tab />
+      <h1>Slider</h1>
+      <Slider />
+      <h1>Input</h1>
+      <Input />
+      <h1>Dropdown</h1>
+      <Dropdown />
     </div>
   )
 }
